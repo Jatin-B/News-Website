530ffeb12cd18efcb81a04a64093c79185218254
Jatin-B <jbaweja24@gmail.com>
2024-11-15 16:30:18 +0530

    apikey

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.API_KEY;
+const API_KEY = `${API_KEY}`;
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
